a6776290fe54f654a7545e4f83b1a506e0b927a3
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2024-09-25 09:04:44 +0300

    found a way to add multiple divs in a row but now i need figure out how to add it to multiple rows

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const gridContainer = document.querySelector('#container');
-const gridDiv = document.createElement('div');
-gridDiv.classList.add('gridDiv');
-gridContainer.appendChild(gridDiv);
+//const gridDiv = document.createElement('div');
+//gridDiv.classList.add('gridDiv');
+//gridContainer.appendChild(gridDiv);
+
+for (let i = 1; i < 16; i++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('rowDiv');
+    gridContainer.appendChild(rowDiv);
+};
+
+/*for (let i = 1; i < 16; i++) {
+    const columDiv = document.createElement('div');
+    columDiv.classList.add('columDiv');
+    gridContainer.appendChild(columDiv);
+};*/
