a8b8091e5e837608c3d7bb45df4247473ca291a5
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2024-09-27 21:10:09 +0300

    added button and grid  scaling
    and user input

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const gridContainer = document.querySelector('#container');
 
+function gridSize() {
+    const gridSizeBtn = document.querySelector('button');
+    gridSizeBtn.addEventListener('click', () => {
+        prompt('Enter grid size!')
+    }
+    );
+};
 
-function gridCreate() { 
-for (let i = 1; i < 10; i++) {
+
+function gridCreate(size) { 
+    
+
+    for (let i = 1; i < size; i++) {
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('rowDiv');
     gridContainer.appendChild(rowDiv);
     
-    for (let j = 1; j < 10; j++) {
+    for (let j = 1; j < size; j++) {
         const cell = document.createElement('div');
         cell.classList.add('cell');
         rowDiv.appendChild(cell);
     };
 };
 };
-
-gridCreate();
+gridSize()
+gridCreate(13);
