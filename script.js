bc954300fd52b45cedcf4c3c63d436dfc0190fff
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2024-09-27 19:04:15 +0300

    It seems that I grid sizeing right now need to figure out how to do user input

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const gridContainer = document.querySelector('#container');
 
 
-
-for (let i = 1; i < 17; i++) {
+function gridCreate() { 
+for (let i = 1; i < 10; i++) {
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('rowDiv');
     gridContainer.appendChild(rowDiv);
     
-    for (let j = 1; j < 13; j++) {
+    for (let j = 1; j < 10; j++) {
         const cell = document.createElement('div');
         cell.classList.add('cell');
         rowDiv.appendChild(cell);
     };
 };
+};
 
+gridCreate();
